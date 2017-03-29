60d0eb2a89c097ad36a731dfc75cb2f25faf0f9f
Mike Mitrofanov <michael.mitrofanov@gmail.com>
2017-03-29 17:37:44 +0300

    add django-cors-headers

--- trouncetest/settings.py
+++ trouncetest/settings.py
@@ -37,6 +37,8 @@ CORE_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'corsheaders'
 ]
 
 CUSTOM_APPS = [
@@ -48,12 +50,15 @@ INSTALLED_APPS = CORE_APPS + CUSTOM_APPS
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
+CORS_ORIGIN_ALLOW_ALL = True
 
 ROOT_URLCONF = 'trouncetest.urls'
 
